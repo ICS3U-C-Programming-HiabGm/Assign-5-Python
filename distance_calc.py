2ce4a362ec88041fe71f93e9bd4bae4b7a579193
hiab gebremichael <hiab.gebremichael.stu.ocsb.ca>
2025-06-11 22:57:42 +0000

    distance_calc.py

--- distance_calc.py
+++ distance_calc.py
@@ -1,11 +1,12 @@
-
 import math
 
+
+# function to calculate distance using distance formula
 def calculate_distance(x1, y1, x2, y2):
-    
-    #Distance formula
-    distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-    
+
+    # Distance formula
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
     return distance
 
 
@@ -17,10 +18,10 @@ def main():
         x2 = float(input("Enter x2: "))
         y2 = float(input("Enter y2: "))
 
-        #checks if the given points are the same
-        if (x1 != x2 or y1 != y2):
-            #checks if the given points are equal to 0
-            if (x1 != 0 or y1 != 0 or x2 != 0 or y2 != 0):
+        # checks if the given points are the same
+        if x1 != x2 or y1 != y2:
+            # checks if the given points are equal to 0
+            if x1 != 0 or y1 != 0 or x2 != 0 or y2 != 0:
                 distance = calculate_distance(x1, y1, x2, y2)
                 print(f"Distance: {calculate_distance(x1, y1, x2, y2)}")
             else:
@@ -34,5 +35,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
